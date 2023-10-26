a1ca7760bc63efa643975efb3a0aef5361c2dc99
Corentin Martin <corentin.mrtn@yahoo.com>
2023-10-26 11:52:31 +0200

    welcome and description

--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -1,25 +1,35 @@
 import './Welcome.scss'
-import {motion, useScroll} from 'framer-motion';
+import {motion} from 'framer-motion';
 import profilPicture from '../../assets/pictures/profil-picture.jpg'
+import AnimatedName from './AnimatedName/AnimatedName';
 
 function Welcome() {
-    const { scrollYProgress } = useScroll()
    
     return (
-        <motion.div initial={{ opacity: 0 }}
-        whileInView={{ opacity: 1 }}
-        viewport={{ once: true }}
-         className="Welcome">
-            <motion.img  whileHover={{ scale: 0.8, borderRadius: '100%' }}
-            
-            
-   className='Welcome-Picture' src={profilPicture} />
-
-
+        <motion.div 
+            initial={{ opacity: 0, scale : 0 }}
+            whileInView={{ opacity: 1, scale: 1 }}
+            transition={{opacity: {duration: 1}, scale: {duration: 1}}}
+            viewport={{ once: true }}
+            className="Welcome"
+        >
+            <motion.img 
+                animate={
+                    {
+                        scale: [1, 2, 2, 1, 1],
+                        rotate: [0, 180, 270, 380, 0],
+                        borderRadius: ["20%", "20%", "50%", "50%", "20%"],
+                    }
+                }   
+                className='Welcome-Picture' src={profilPicture} 
+            />
             <div className='Welcome-Box'>
-            <p>Hello, je suis</p>
-            <motion.h1 className='Welcome-Box-Title'>Corentin MARTIN</motion.h1>
-            <h2 className='Welcome-Box-H2'>Développeur web & web-mobile</h2>
+                <p>Hello, je suis</p>
+                <div className='Welcome-Box-Name'>
+                    <AnimatedName text="Corentin" />
+                    <AnimatedName text="MARTIN" />
+                </div>
+                <h2 className='Welcome-Box-H2'>Développeur web & web-mobile</h2>
             </div>
         </motion.div>
     )
